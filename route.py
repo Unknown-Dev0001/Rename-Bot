76bcd8a7a86f14eeca1275714a65398cae973b8e
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-06-25 14:52:57 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,20 +4,10 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("JishuBotz")
+    return web.json_response("Bot chal raha hai @BotVerseRavi")
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @JishuBotz
-# Developer @JishuDeveloper
